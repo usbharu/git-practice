927d21cd6aa68947d26483a89e174f439a3a0e0a
usbharu <i@usbharu.dev>
2025-07-17 00:54:07 +0900

    style: fix lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,29 @@
-"use client"
-import Image from "next/image";
-import {useState} from "react";
+"use client";
+import { useState } from "react";
 export default function Home() {
+	const add = (a: number, b: number): number => {
+		return a + b;
+	};
 
-    const add = (a: number, b: number): number => {
-        return a + b;
-    }
+	const [a, setA] = useState(0);
+	const [b, setB] = useState(0);
 
-    const [a, setA] = useState(0)
-    const [b, setB] = useState(0)
-
-
-    return (
-        <div>
-            <form className="m-2">
-                <input className="border-2 inline-block" type="number" onChange={event => setA(Number(event.target.value))}/>
-                <p className="inline-block">+</p>
-                <input className="border-2" type="number" onChange={event => setB(Number(event.target.value))}/>
-                <p className="inline-block">={add(a, b)}</p>
-            </form>
-        </div>
-    );
+	return (
+		<div>
+			<form className="m-2">
+				<input
+					className="border-2 inline-block"
+					type="number"
+					onChange={(event) => setA(Number(event.target.value))}
+				/>
+				<p className="inline-block">+</p>
+				<input
+					className="border-2"
+					type="number"
+					onChange={(event) => setB(Number(event.target.value))}
+				/>
+				<p className="inline-block">={add(a, b)}</p>
+			</form>
+		</div>
+	);
 }
