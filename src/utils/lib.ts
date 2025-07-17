7ac1766254c712e189006e959d4e66984d07b78f
usbharu <i@usbharu.dev>
2025-07-17 14:31:57 +0900

    引き算を追加

--- src/utils/lib.ts
+++ src/utils/lib.ts
@@ -2,6 +2,6 @@ export const add = (a: number, b: number): number => {
     return a + b;
 };
 
-// export const sub = (a: number, b: number): number => {
-//     return a - b;
-// }
+export const sub = (a: number, b: number): number => {
+    return a - b;
+}
