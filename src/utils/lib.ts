1d1ebc456ac94760e886173382f21ee7f815d9f1
yuma0820 <yuma.karepan@icloud.com>
2025-07-17 14:46:52 +0900

    割り算を追加

--- src/utils/lib.ts
+++ src/utils/lib.ts
@@ -5,3 +5,7 @@ export const add = (a: number, b: number): number => {
 export const sub = (a: number, b: number): number => {
     return a - b;
 }
+
+export const div = (a: number, b: number): number => {
+    return a / b;
+}
